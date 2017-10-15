2219a212d0fa5443c5b0a5f545cec4ddbb9fb541
kvlt <remillary@gmail.com>
2017-10-15 17:57:43 +0600

    rm unused

--- DartaBungee/src/net/enot/dartabungee/auth/AuthManager.java
+++
@@ -1,7 +0,0 @@
-package net.enot.dartabungee.auth;
-
-/**
- * Created by Енот on 12.05.2017.
- */
-public class AuthManager {
-}
--- DartaBungee/src/net/enot/dartabungee/utils/BungeeUtil.java
+++
@@ -1,8 +0,0 @@
-package net.enot.dartabungee.utils;
-
-/**
- * Created by Енот on 12.05.2017.
- */
-public class BungeeUtil {
-
-}
