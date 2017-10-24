a2cfb213394f333e938acdc155b811a0271d2fe1
kvlt <remillary@gmail.com>
2017-10-24 17:15:48 +0700

    stream using

--- CoreCLI/src/org/kvlt/core/OnlinePlayers.java
+++ CoreCLI/src/org/kvlt/core/OnlinePlayers.java
@@ -8,25 +8,12 @@ import java.util.LinkedList;
 public class OnlinePlayers extends ArrayList<OnlinePlayer> {
 
     /**
-     * Ищет по всему списку подходящего по UUID и имени игрока и удаляет его
+     * Ищет по всему списку подходящего по имени игрока и удаляет его
      */
     @Override
     public boolean remove(Object o) {
         OnlinePlayer target = (OnlinePlayer) o;
-        for (OnlinePlayer p : this) {
-            boolean sameName = p.getName().equalsIgnoreCase(target.getName());
-            boolean sameUUID = p.getUUID().equals(target.getUUID());
-
-            if (sameUUID) {
-                if (sameName) {
-                    super.remove(p);
-                    return true;
-                }
-                break;
-            }
-
-        }
-        return false;
+        return removeIf(p -> p.getName().equalsIgnoreCase(target.getName()));
     }
 
 }
